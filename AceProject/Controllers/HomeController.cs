c11b989055f2d94f1fd3979e9a0e63beaa3679d6
Pragatheeshkumar <pragatheeshkumar@gmail.com>
2012-12-06 13:49:54 +0530

    Signed-off-by: Pragatheeshkumar <pragatheeshkumar@gmail.com>

--- AceProject/Controllers/HomeController.cs
+++ AceProject/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AceProject.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to RTBS";
 
             return View();
         }
